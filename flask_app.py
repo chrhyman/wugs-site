84358eea56f904e86357cdd64dbf7c987d6b4f2e
Chris Hyman <chrhyman@gmail.com>
2016-07-06 19:45:23 +0000

    New CSS and an image...we growin up fam

--- flask_app.py
+++ flask_app.py
@@ -1,32 +1,8 @@
 from flask import Flask, render_template
-#from flask import redirect, request, url_for
-#from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-#sql = "mysql+mysqlconnector://{0}:{1}@{2}/{3}".format("wugs", "sqlpassword",
-#    "wugs.mysql.pythonanywhere-services.com", "wugs$<DATABASE>")
-#app.config["SQLALCHEMY_DATABASE_URI"] = sql
-#app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-
-#db = SQLAlchemy(app)
-
-#class Comment(db.Model):
-#
-#    __tablename__ = "comments"
-#
-#    id = db.Column(db.Integer, primary_key=True)
-#    content = db.Column(db.String(4096))
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('main_page.html')
-
-#    if request.method == 'GET':
-#        return render_template('main_page.html', comments=Comment.query.all())
-#
-#    comment = Comment(content=request.form["contents"])
-#    db.session.add(comment)
-#    db.session.commit()
-#    return redirect(url_for('index'))
